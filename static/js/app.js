fe8a79da67178dd15cd29321892c0f760b897d67
Tomek Felisiak <tomekfel@googlemail.com>
2018-02-27 21:18:16 +0000

    data ok, img missing

---
+++ static/js/app.js
@@ -0,0 +1,42 @@
+let CRYPTOCOMPARE_API_URI = "https://min-api.cryptocompare.com";
+let COINMARKETCAP_API_URI = "https://api.coinmarketcap.com";
+let CRYPTOCOMPARE_IMG_URI = "https://www.cryptocompare.com";
+
+let UPDATE_INTERVAL = 60 * 1000;
+
+let app = new Vue({
+    el: "#app",
+    data: {
+        coins: [],
+        coinData: {}
+    },
+    methods: {
+        getCoinData: function() {
+            let self = this;
+            axios.get(CRYPTOCOMPARE_API_URI + "/data/all/coinlist")
+                .then((resp) => {
+                    this.coinData = resp.data.Data;
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        },
+        getCoins: function() {
+            let self = this;
+            axios.get(COINMARKETCAP_API_URI + "/v1/ticker/?limit=10")
+                .then((resp) => {
+                    this.coins = resp.data;
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        },
+        getCoinImage: function(symbol) {
+            console.log(CRYPTOCOMPARE_API_URI + this.coinData[symbol].ImageUrl);
+            return CRYPTOCOMPARE_IMG_URI + this.coinData[symbol].ImageUrl;
+        }
+    },
+    created: function() {
+        this.getCoinData();
+    }
+});
